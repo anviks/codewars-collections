1156d77496e4c8d8fc67e1ce4957e5b34a3063ae
anviks <anviks@ttu.ee>
2024-08-05 17:54:29 +0300

    "Reorganize 3kyu kata files"

--- 4kyu/sum_by_factors/solution_sum_by_factors.py
+++
@@ -1,5 +0,0 @@
-"""https://www.codewars.com/kata/54d496788776e49e6b00052f"""
-
-
-def sum_for_list(lst):
-    return None
--- 4kyu/sum_of_intervals/solution_sum_of_intervals.py
+++
@@ -1,5 +0,0 @@
-"""https://www.codewars.com/kata/52b7ed099cdc285c300001cd"""
-
-
-def sum_of_intervals(intervals):
-    pass
