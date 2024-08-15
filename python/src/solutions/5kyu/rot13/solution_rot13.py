66099ad676cb6147663365c80fe96e3ff9616772
anviks <anviks@ttu.ee>
2024-08-16 00:49:07 +0300

    Rename directories using codewars api given slug instead of using regex on kata's title

--- python/src/solutions/5kyu/rot13/solution_rot13.py
+++ python/src/solutions/5kyu/rot13/solution_rot13.py
@@ -1,20 +1,3 @@
-"""https://www.codewars.com/kata/530e15517bc88ac656000716"""
+"""https://www.codewars.com/kata/52223df9e8f98c7aa7000062"""
 
-
-def rot13(message: str):
-    new_msg = ''
-
-    for char in message:
-        if char.isupper():
-            first_char = 'A'
-        elif char.islower():
-            first_char = 'a'
-        else:
-            new_msg += char
-            continue
-
-        old_pos = ord(char) - ord(first_char)
-        new_pos = (old_pos + 13) % 26 + ord(first_char)
-        new_msg += chr(new_pos)
-
-    return new_msg
+rot13 = lambda s: __import__('codecs').encode(s, 'rot13')
--- python/src/solutions/5kyu/rot13/test_rot13.py
+++ python/src/solutions/5kyu/rot13/test_rot13.py
@@ -1,12 +1,16 @@
-"""https://www.codewars.com/kata/530e15517bc88ac656000716"""
+"""https://www.codewars.com/kata/52223df9e8f98c7aa7000062"""
 import unittest
 
 from solution_rot13 import rot13
 
 
-class FixedTests(unittest.TestCase):
-    def test_should_obtain_correct_rot13_encoding_on_fixed_strings(self):
-        self.assertEqual(rot13('test'), 'grfg', 'Returned solution incorrect for fixed string = test')
-        self.assertEqual(rot13('Test'), 'Grfg', 'Returned solution incorrect for fixed string = Test')
-        self.assertEqual(rot13('aA bB zZ 1234 *!?%'), 'nN oO mM 1234 *!?%',
-                         'Returned solution incorrect for fixed string = aA bB zZ 1234 *!?%')
+class ExampleTests(unittest.TestCase):
+    def test_rot13(self):
+        self.assertEqual(rot13("EBG13 rknzcyr."), "ROT13 example.")
+        self.assertEqual(rot13(
+            "How can you tell an extrovert from an\nintrovert at NSA? Va gur ryringbef,\ngur rkgebireg ybbxf ng gur BGURE thl'f fubrf."),
+                         "Ubj pna lbh gryy na rkgebireg sebz na\nvagebireg ng AFN? In the elevators,\nthe extrovert looks at the OTHER guy's shoes.")
+        self.assertEqual(rot13("123"), "123")
+        self.assertEqual(rot13("Guvf vf npghnyyl gur svefg xngn V rire znqr. Gunaxf sbe svavfuvat vg! :)"),
+                         "This is actually the first kata I ever made. Thanks for finishing it! :)")
+        self.assertEqual(rot13("@[`{"), "@[`{")
