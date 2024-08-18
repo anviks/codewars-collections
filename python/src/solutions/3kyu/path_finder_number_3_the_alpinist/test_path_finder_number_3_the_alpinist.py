359643c72bda10aa6777237179cc143fa92fe9ae
anviks <anviks@ttu.ee>
2024-08-18 14:57:15 +0300

    Use pytest instead of codewars_test and unittest

--- python/src/solutions/3kyu/path_finder_number_3_the_alpinist/test_path_finder_number_3_the_alpinist.py
+++ python/src/solutions/3kyu/path_finder_number_3_the_alpinist/test_path_finder_number_3_the_alpinist.py
@@ -1,68 +1,20 @@
 """https://www.codewars.com/kata/576986639772456f6f00030c"""
 
-import unittest
-
-from solution_path_finder_3_the_alpinist import path_finder
+from solution_path_finder_number_3_the_alpinist import *
 
 
-class FixedTests(unittest.TestCase):
-    def test_basic_test_cases(self):
-        a = "\n".join([
-            "000",
-            "000",
-            "000"
-        ])
-
-        b = "\n".join([
-            "010",
-            "010",
-            "010"
-        ])
-
-        c = "\n".join([
-            "010",
-            "101",
-            "010"
-        ])
-
-        d = "\n".join([
-            "0707",
-            "7070",
-            "0707",
-            "7070"
-        ])
-
-        e = "\n".join([
-            "700000",
-            "077770",
-            "077770",
-            "077770",
-            "077770",
-            "000007"
-        ])
-
-        f = "\n".join([
-            "777000",
-            "007000",
-            "007000",
-            "007000",
-            "007000",
-            "007777"
-        ])
-
-        g = "\n".join([
-            "000000",
-            "000000",
-            "000000",
-            "000010",
-            "000109",
-            "001010"
-        ])
-
-        self.assertEqual(path_finder(a), 0)
-        self.assertEqual(path_finder(b), 2)
-        self.assertEqual(path_finder(c), 4)
-        self.assertEqual(path_finder(d), 42)
-        self.assertEqual(path_finder(e), 14)
-        self.assertEqual(path_finder(f), 0)
-        self.assertEqual(path_finder(g), 4)
+def test_fixed_tests__basic_test_cases():
+    a = '\n'.join(['000', '000', '000'])
+    b = '\n'.join(['010', '010', '010'])
+    c = '\n'.join(['010', '101', '010'])
+    d = '\n'.join(['0707', '7070', '0707', '7070'])
+    e = '\n'.join(['700000', '077770', '077770', '077770', '077770', '000007'])
+    f = '\n'.join(['777000', '007000', '007000', '007000', '007000', '007777'])
+    g = '\n'.join(['000000', '000000', '000000', '000010', '000109', '001010'])
+    assert path_finder(a) == 0
+    assert path_finder(b) == 2
+    assert path_finder(c) == 4
+    assert path_finder(d) == 42
+    assert path_finder(e) == 14
+    assert path_finder(f) == 0
+    assert path_finder(g) == 4
--- python/src/solutions/3kyu/roboscript_number_4_rs3_patterns_to_the_rescue/test_roboscript_number_4_rs3_patterns_to_the_rescue.py
+++ python/src/solutions/3kyu/roboscript_number_4_rs3_patterns_to_the_rescue/test_roboscript_number_4_rs3_patterns_to_the_rescue.py
@@ -1,58 +1,63 @@
 """https://www.codewars.com/kata/594b898169c1d644f900002e"""
-import unittest
 
-from parameterized import parameterized
+import pytest
 
-from solution_roboscript_4_rs3_patterns_to_the_rescue import execute
+from solution_roboscript_number_4_rs3_patterns_to_the_rescue import *
 
 
-class YourRS3Interpreter(unittest.TestCase):
-    @parameterized.expand([
-        ('should work for RS2-compliant programs', [
-            ('(F2LF2R)2FRF4L(F2LF2R)2(FRFL)4(F2LF2R)2', "    **   **      *\r\n    **   ***     *\r\n  **** *** **  ***\r\n  *  * *    ** *  \r\n***  ***     ***  ")
-        ]),
-        ('should properly parse a pattern definition and not cause any side effects', [
-            ('p0(F2LF2R)2q', '*'),
-            ('p312(F2LF2R)2q', '*')
-        ]),
-        ('should execute a given pattern when it is invoked', [
-            ('p0(F2LF2R)2qP0', "    *\r\n    *\r\n  ***\r\n  *  \r\n***  "),
-            ('p312(F2LF2R)2qP312', "    *\r\n    *\r\n  ***\r\n  *  \r\n***  ")
-        ]),
-        ('should always parse pattern definitions first before attempting to invoke them', [
-            ('P0p0(F2LF2R)2q', "    *\r\n    *\r\n  ***\r\n  *  \r\n***  "),
-            ('P312p312(F2LF2R)2q', "    *\r\n    *\r\n  ***\r\n  *  \r\n***  ")
-        ]),
-        ('should allow other forms of RoboScript code alongside pattern definitions and invocations', [
-            ('F3P0Lp0(F2LF2R)2qF2', "       *\r\n       *\r\n       *\r\n       *\r\n     ***\r\n     *  \r\n******  ")
-        ]),
-        ('should allow a pattern to be invoked multiple times', [
-            ('(P0)2p0F2LF2RqP0', "      *\r\n      *\r\n    ***\r\n    *  \r\n  ***  \r\n  *    \r\n***    ")
-        ]),
-        ('should properly parse multiple pattern definitions', [
-            ('P1P2p1F2Lqp2F2RqP2P1', "  ***\r\n  * *\r\n*** *"),
-            ('p1F2Lqp2F2Rqp3P1(P2)2P1q(P3)3', "  *** *** ***\r\n  * * * * * *\r\n*** *** *** *")
-        ]),
-    ])
-    def test_should_pass(self, msg, test_cases):
-        for code, expected in test_cases:
-            self.assertEqual(expected, execute(code), msg)
+def test_your_rs3_interpreter():
+
+    def assert_path_equals(actual, expected):
+        if actual != expected:
+            print('You returned:')
+            print(actual)
+            print('Expected path of MyRobot:')
+            print(expected)
+        assert actual == expected
+
+    def expect_error(why, code):
+        with pytest.raises(Exception):
+            execute(code)
+
+    tests = [('should work for RS2-compliant programs', [
+        lambda: assert_path_equals(execute('(F2LF2R)2FRF4L(F2LF2R)2(FRFL)4(F2LF2R)2'),
+                                   '    **   **      *\r\n    **   ***     *\r\n  **** *** **  ***\r\n  *  * *    ** *  \r\n***  ***     ***  ')]),
+             ('should properly parse a pattern definition and not cause any side effects',
+              [lambda: assert_path_equals(execute('p0(F2LF2R)2q'), '*'),
+               lambda: assert_path_equals(execute('p312(F2LF2R)2q'), '*')]), (
+             'should execute a given pattern when it is invoked',
+             [lambda: assert_path_equals(execute('p0(F2LF2R)2qP0'), '    *\r\n    *\r\n  ***\r\n  *  \r\n***  '),
+              lambda: assert_path_equals(execute('p312(F2LF2R)2qP312'), '    *\r\n    *\r\n  ***\r\n  *  \r\n***  ')]),
+             ('should always parse pattern definitions first before attempting to invoke them',
+              [lambda: assert_path_equals(execute('P0p0(F2LF2R)2q'), '    *\r\n    *\r\n  ***\r\n  *  \r\n***  '),
+               lambda: assert_path_equals(execute('P312p312(F2LF2R)2q'), '    *\r\n    *\r\n  ***\r\n  *  \r\n***  ')]),
+             ('should allow other forms of RoboScript code alongside pattern definitions and invocations', [
+                 lambda: assert_path_equals(execute('F3P0Lp0(F2LF2R)2qF2'),
+                                            '       *\r\n       *\r\n       *\r\n       *\r\n     ***\r\n     *  \r\n******  ')]),
+             ('should allow a pattern to be invoked multiple times', [
+                 lambda: assert_path_equals(execute('(P0)2p0F2LF2RqP0'),
+                                            '      *\r\n      *\r\n    ***\r\n    *  \r\n  ***  \r\n  *    \r\n***    ')]),
+             ('should throw an error when a non-existing pattern is invoked', [
+                 lambda: expect_error('Your interpreter should throw an error because pattern "P1" does not exist',
+                                      'p0(F2LF2R)2qP1'),
+                 lambda: expect_error('Your interpreter should throw an error because pattern "P0" does not exist',
+                                      'P0p312(F2LF2R)2q'),
+                 lambda: expect_error('Your interpreter should throw an error because pattern "P312" does not exist',
+                                      'P312')]), ('should properly parse multiple pattern definitions', [
+            lambda: assert_path_equals(execute('P1P2p1F2Lqp2F2RqP2P1'), '  ***\r\n  * *\r\n*** *'),
+            lambda: assert_path_equals(execute('p1F2Lqp2F2Rqp3P1(P2)2P1q(P3)3'),
+                                       '  *** *** ***\r\n  * * * * * *\r\n*** *** *** *')]), (
+             'should throw an error when a pattern is defined more than once', [
+                 lambda: expect_error('Your interpreter should throw an error since pattern "P1" is defined twice',
+                                      'p1F2Lqp1(F3LF4R)5qp2F2Rqp3P1(P2)2P1q(P3)3')]), (
+             'should throw an error when any form of infinite recursion is detected', [
+                 lambda: expect_error('should throw an error when any form of infinite recursion is detected',
+                                      'p1F2RP1F2LqP1'), lambda: expect_error(
+                     'Your interpreter should throw an error since pattern "P1" invokes "P2" which then again invokes "P1", creating an infinite cycle',
+                     'p1F2LP2qp2F2RP1qP1')])]
+    
+    for scenario in tests:
+        message, assertions = scenario
         
-    @parameterized.expand([
-        ('should throw an error when a non-existing pattern is invoked', [
-            ('Your interpreter should throw an error because pattern "P1" does not exist', 'p0(F2LF2R)2qP1'),
-            ('Your interpreter should throw an error because pattern "P0" does not exist', 'P0p312(F2LF2R)2q'),
-            ('Your interpreter should throw an error because pattern "P312" does not exist', 'P312')
-        ]),
-        ('should throw an error when a pattern is defined more than once', [
-            ('Your interpreter should throw an error since pattern "P1" is defined twice', 'p1F2Lqp1(F3LF4R)5qp2F2Rqp3P1(P2)2P1q(P3)3')
-        ]),
-        ('should throw an error when any form of infinite recursion is detected', [
-            ('should throw an error when any form of infinite recursion is detected', 'p1F2RP1F2LqP1'),
-            ('Your interpreter should throw an error since pattern "P1" invokes "P2" which then again invokes "P1", creating an infinite cycle', 'p1F2LP2qp2F2RP1qP1')
-        ])
-    ])
-    def test_should_fail(self, _, test_cases):
-        for why, code in test_cases:
-            with self.assertRaises(Exception) as context:
-                execute(code)
+        for assertion in assertions:
+            assertion()
