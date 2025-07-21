2ecf18a382b8747739baa7a67d61377a3e6d7457
anviks <anviks@ttu.ee>
2025-07-21 16:18:51 +0300

    Add string comparison macro

--- c/src/test_macros.hpp
+++ c/src/test_macros.hpp
@@ -1,9 +1,24 @@
 #ifndef CODEWARS_C_TEST_MACROS_HPP
 #define CODEWARS_C_TEST_MACROS_HPP
 
-#define REQUIRE_MSG(expr, msg) INFO(msg); REQUIRE(expr);
-#define REQUIRE_FALSE_MSG(expr, msg) INFO(msg); REQUIRE_FALSE(expr);
-#define CHECK_MSG(expr, msg) INFO(msg); CHECK(expr);
-#define CHECK_FALSE_MSG(expr, msg) INFO(msg); CHECK_FALSE(expr);
+#define REQUIRE_MSG(expr, msg)                                                                                         \
+    INFO(msg);                                                                                                         \
+    REQUIRE(expr);
 
-#endif //CODEWARS_C_TEST_MACROS_HPP
+#define REQUIRE_FALSE_MSG(expr, msg)                                                                                   \
+    INFO(msg);                                                                                                         \
+    REQUIRE_FALSE(expr);
+
+#define CHECK_MSG(expr, msg)                                                                                           \
+    INFO(msg);                                                                                                         \
+    CHECK(expr);
+
+#define CHECK_FALSE_MSG(expr, msg)                                                                                     \
+    INFO(msg);                                                                                                         \
+    CHECK_FALSE(expr);
+
+#define REQUIRE_STR_EQ(str1, str2)                                                                                     \
+    INFO("Comparing strings: \"" << str1 << "\" and \"" << str2 << "\"");                                              \
+    REQUIRE(strcmp(str1, str2) == 0);
+
+#endif  // CODEWARS_C_TEST_MACROS_HPP
